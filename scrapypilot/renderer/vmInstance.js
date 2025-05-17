f8cac79f81392ba4c02c27d8ec5d8cf8b69270b5
Ayush <ayushsh285@gmail.com>
2025-05-16 23:15:30 -0400

    refactor

--- scrapypilot/renderer/vmInstance.js
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-document.getElementById('openManager')?.addEventListener('click', () => {
-    console.log("OPENING IN vmInstance.ts");
-    ipcRenderer.send('open-manager-window');
-});
